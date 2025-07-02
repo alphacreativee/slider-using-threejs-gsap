920e3ccbd9845e3bdd72bea75f556fe16c7e6b5c
Gary <hai.alphacreative@gmail.com>
2025-07-02 13:39:04 +0700

    up source

---
+++ assets/libs/slides.js
@@ -0,0 +1,29 @@
+export const slides = [
+  {
+    title: "HTML",
+    description:
+      "HTML (HyperText Markup Language) is the standard markup language for creating web",
+    type: "html",
+    field: "html",
+    date: "Jun 24, 2025",
+    image: "./assets/images/1.jpg",
+  },
+  {
+    title: "CSS",
+    description:
+      "HTML (HyperText Markup Language) is the standard markup language for creating web",
+    type: "html",
+    field: "html",
+    date: "Jun 24, 2025",
+    image: "./assets/images/3.jpg",
+  },
+  {
+    title: "JS",
+    description:
+      "HTML (HyperText Markup Language) is the standard markup language for creating web",
+    type: "html",
+    field: "html",
+    date: "Jun 24, 2025",
+    image: "./assets/images/2.jpg",
+  },
+];
